3c7bfc63c41d0aae36cbe33eaafcaaa722942fda
VINICIUS HENRIQUE <132083910+henviniv@users.noreply.github.com>
2025-06-04 21:27:52 +0000

    atualização de css

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,10 @@ from flask import Flask
 from app.routes import bp
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(
+        __name__,
+        static_folder='static',          # pasta onde está style.css
+        template_folder='templates'      # pasta onde está index.html
+    )
     app.register_blueprint(bp)
     return app

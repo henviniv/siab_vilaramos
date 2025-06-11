8ba32a39f2f862bca89c3ae7fc790efb8a99bcfa
VINICIUS HENRIQUE <132083910+henviniv@users.noreply.github.com>
2025-06-11 19:55:52 +0000

    atualiza init

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,19 @@
 from flask import Flask
+from flask_login import LoginManager
 from app.routes import bp
+from app.auth import User, USERS  # User e USERS do seu auth.py
 import re
+
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(
         __name__,
-        static_folder='static',      # pasta onde está style.css
-        template_folder='templates'  # pasta onde está index.html
+        static_folder='static',
+        template_folder='templates'
     )
+
+    app.secret_key = '77f6345fdced536f3f36e2638e14fedf'  # Troque por uma chave segura
 
     # Filtro Jinja para limpar CPF
     @app.template_filter('limpar_cpf')
@@ -15,5 +21,16 @@ def create_app():
         return re.sub(r'\D', '', cpf or "")
 
     app.register_blueprint(bp)
+
+    # Configuração do Flask-Login
+    login_manager.init_app(app)
+    login_manager.login_view = 'main.login'  # Rota usada para redirecionar se o usuário não estiver logado
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        if user_id in USERS:
+            info = USERS[user_id]
+            return User(id=user_id, username=user_id, role=info["role"], aba=info["aba"])
+        return None
 
     return app

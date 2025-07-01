dece7c36ae6f25507bdd11a301fe2ece93cb3922
VINICIUS HENRIQUE <132083910+henviniv@users.noreply.github.com>
2025-07-01 12:23:45 +0000

    permite admin fazer pesquisas com query

--- app/routes.py
+++ app/routes.py
@@ -25,6 +25,8 @@ def num_to_col(n):
 @login_required
 def index():
     # Só verifica o papel se o usuário estiver autenticado (garantido por @login_required)
+    if current_user.is_authenticated and current_user.role == "admin" and not request.args.get("query"):
+        return redirect(url_for("main.painel_admin"))
 
     try:
         print(f"[DEBUG] Usuário: {current_user.username}, aba: {current_user.aba}")

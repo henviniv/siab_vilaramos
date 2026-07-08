affef9eb073e99d5cc41b3d6d68f44aecac066a0
VINICIUS HENRIQUE <132083910+henviniv@users.noreply.github.com>
2026-07-08 03:04:03 +0000

    arruma init

--- app/__init__.py
+++ app/__init__.py
@@ -1,36 +1,56 @@
 from flask import Flask
 from flask_login import LoginManager
 from app.routes import bp
-from app.auth import User, USERS  # User e USERS do seu auth.py
+from app.auth import User, USERS
 import re
 
 login_manager = LoginManager()
 
+
 def create_app():
+
     app = Flask(
         __name__,
         static_folder='static',
         template_folder='templates'
     )
 
-    app.secret_key = '77f6345fdced536f3f36e2638e14fedf'  
+    app.secret_key = '77f6345fdced536f3f36e2638e14fedf'
+
 
     # Filtro Jinja para limpar CPF
     @app.template_filter('limpar_cpf')
     def limpar_cpf_filter(cpf):
         return re.sub(r'\D', '', cpf or "")
 
+
+    # Blueprint principal
     app.register_blueprint(bp)
 
-    # Configuração do Flask-Login
-    login_manager.init_app(app)
-    login_manager.login_view = 'main.login'  # Rota usada para redirecionar se o usuário não estiver logado
 
+    # Flask Login
+    login_manager.init_app(app)
+
+    login_manager.login_view = 'main.login'
+
+
+    # Recupera usuário salvo na sessão
     @login_manager.user_loader
     def load_user(user_id):
-        if user_id in USERS:
-            info = USERS[user_id]
-            return User(id=user_id, username=user_id, role=info["role"], aba=info["aba"], planilha=info["planilha"])
+
+        info = USERS.get(user_id)
+
+        if info:
+
+            return User(
+                id=user_id,
+                username=user_id,
+                role=info["role"],
+                micro=info.get("micro"),
+                equipe=info.get("equipe")
+            )
+
         return None
+
 
     return app

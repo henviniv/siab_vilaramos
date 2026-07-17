522c795c8ed6a22b803a814f273f433309d24d8d
VINICIUS HENRIQUE <132083910+henviniv@users.noreply.github.com>
2026-07-17 13:39:02 +0000

    retirar user do auth.py e arruma select(*)

--- app/auth.py
+++ app/auth.py
@@ -15,45 +15,7 @@ class User(UserMixin):
         self.micro = micro
         self.equipe = equipe
 
-# Dicionário de usuários
-USERS = {
-    "admin": {
-        "password": "ramos2025",
-        "role": "admin",
-        "micro": None,
-        "equipe": None
-    },
-    "micro01": {"password": "eli1611", "role": "micro", "micro": "MI 01", "equipe": "EQUIPE 1"},
-    "micro02": {"password": "585283vb", "role": "micro", "micro": "MI 02", "equipe": "EQUIPE 1"},
-    "micro03": {"password": "miewag7681", "role": "micro", "micro": "MI 03", "equipe": "EQUIPE 1"},
-    "micro04": {"password": "920795g", "role": "micro", "micro": "MI 04", "equipe": "EQUIPE 1"},
-    "micro05": {"password": "134579", "role": "micro", "micro": "MI 05", "equipe": "EQUIPE 1"},
-    "micro06": {"password": "gigu0106", "role": "micro", "micro": "MI 06", "equipe": "EQUIPE 1"},
-    "micro07": {"password": "223344ma", "role": "micro", "micro": "MICRO 7", "equipe": "EQUIPE 2"},
-    "micro08": {"password": "r4828", "role": "micro", "micro": "MICRO 8", "equipe": "EQUIPE 2"},
-    "micro09": {"password": "Samuel2025", "role": "micro", "micro": "MICRO 9", "equipe": "EQUIPE 2"},
-    "micro10": {"password": "manuella3004", "role": "micro", "micro": "MICRO 10", "equipe": "EQUIPE 2"},
-    "micro11": {"password": "Mar617318", "role": "micro", "micro": "MICRO 11", "equipe": "EQUIPE 2"},
-    "micro12": {"password": "a95792315", "role": "micro", "micro": "MICRO 12", "equipe": "EQUIPE 2"},
-    "micro13": {"password": "balada10", "role": "micro", "micro": "MICRO 13", "equipe": "EQUIPE 3"},
-    "micro14": {"password": "96063404m", "role": "micro", "micro": "MICRO 14", "equipe": "EQUIPE 3"},
-    "micro15": {"password": "4303li", "role": "micro", "micro": "MICRO 15", "equipe": "EQUIPE 3"},
-    "micro16": {"password": "RE00316#", "role": "micro", "micro": "MICRO 16", "equipe": "EQUIPE 3"},
-    "micro17": {"password": "de123", "role": "micro", "micro": "MICRO 17", "equipe": "EQUIPE 3"},
-    "micro18": {"password": "@vr12345", "role": "micro", "micro": "MICRO 18", "equipe": "EQUIPE 3"},
-    "micro19": {"password": "Monic@123", "role": "micro", "micro": "MICRO 19", "equipe": "EQUIPE 4"},
-    "micro20": {"password": "senha20", "role": "micro", "micro": "MICRO 20", "equipe": "EQUIPE 4"},
-    "micro21": {"password": "Na130689", "role": "micro", "micro": "MICRO 21", "equipe": "EQUIPE 4"},
-    "micro22": {"password": "senha22", "role": "micro", "micro": "MICRO 22", "equipe": "EQUIPE 4"},
-    "micro23": {"password": "Hidek_22", "role": "micro", "micro": "MICRO 23", "equipe": "EQUIPE 4"},
-    "micro24": {"password": "30242202Ra", "role": "micro", "micro": "MICRO 24", "equipe": "EQUIPE 4"},
-    "micro25": {"password": "419905", "role": "micro", "micro": "MICRO 25", "equipe": "EQUIPE 5"},
-    "micro26": {"password": "123preto", "role": "micro", "micro": "MICRO 26", "equipe": "EQUIPE 5"},
-    "micro27": {"password": "Gis512629", "role": "micro", "micro": "MICRO 27", "equipe": "EQUIPE 5"},
-    "micro28": {"password": "Luna@2030", "role": "micro", "micro": "MICRO 28", "equipe": "EQUIPE 5"},
-    "micro29": {"password": "NAnu4483", "role": "micro", "micro": "MICRO 29", "equipe": "EQUIPE 5"},
-    "micro30": {"password": "110893He", "role": "micro", "micro": "MICRO 30", "equipe": "EQUIPE 5"},
-}
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -61,7 +23,7 @@ def load_user(user_id):
         resposta = (
             supabase
             .table("usuarios")
-            .select("*")
+            .select("username, role, micro, equipe")
             .eq("username", user_id)
             .execute()
         )
